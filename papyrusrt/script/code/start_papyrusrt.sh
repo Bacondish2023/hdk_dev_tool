4e7371eca9ff01e9f39440dd5e0c732588afc21c
Hidekazu TAKAHASHI <139677991+Bacondish2023@users.noreply.github.com>
2026-09-01 22:27:37 +0900

    Release Version 1.2.1

--- papyrusrt/script/code/start_papyrusrt.sh
+++ papyrusrt/script/code/start_papyrusrt.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2026 Hidekazu TAKAHASHI
 # SPDX-License-Identifier: MIT
 # Source: https://github.com/Bacondish2023/hdk_dev_tool
-# Version: 1.2.0
+# Version: 1.2.1
 #
 # This script creates workspace and launches Papyrus-RT
 #
